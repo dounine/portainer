b10b6e067182527d5c2098ca72245ff73ea941cf
dounine <102535481@qq.com>
2017-08-24 02:42:31 -0500

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -9,6 +9,6 @@ token=${token:1}
 token=${token%?}
 data={\"Name\":\"$hostname\",\"URL\":\"tcp://$hostname:2376\",\"PublicURL\":\"$ip\",\"TLS\":true}
 uploadId=`curl -l -H "Content-type: application/json" -H "Authorization: $token" -X POST -d $data $host/endpoints | jq ".Id"`
-curl -F "file=@ca.pem" -H "Authorization: $token" $host/upload/tls/$uploadId/ca
-curl -F "file=@cert.pem" -H "Authorization: $token" $host/upload/tls/$uploadId/cert
-curl -F "file=@key.pem" -H "Authorization: $token" $host/upload/tls/$uploadId/key
+curl -F "file=@ca.pem" -H "Authorization: $token" $host/upload/tls/ca?folder=$uploadId
+curl -F "file=@cert.pem" -H "Authorization: $token" $host/upload/tls/cert?folder=$uploadId
+curl -F "file=@key.pem" -H "Authorization: $token" $host/upload/tls/key?folder=$uploadId
