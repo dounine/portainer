6cca0de5b1c32132036cc6609df51ce1e6d3f5c1
lake <102535481@qq.com>
2017-12-02 17:10:59 +0800

    m

--- portainer.sh
+++ portainer.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+# 依赖tls
 domain=$1
 yum install jq net-tools -y
+#hostname=`ifconfig eth0 | grep "inet" | awk '{ print $2}' | sed -n '1p;1q'` #获取主机域名
 hostname=`hostname` #获取主机域名
 ip=`ifconfig eth0 | grep "inet" | awk '{ print $2}' | sed -n '1p;1q'` #获取主机IP地扯
-host=https://$domain/api #portainer地扯
-token=`curl -l -H "Content-type: application/json" -X POST -d '{"username":"admin","password":"xxxPassword"}' $host/auth | jq ".jwt"`
+#host=https://$domain/api #portainer地扯
+host=$domain/api #portainer地扯
+password=`cat password.txt`
+token=`curl -l -H "Content-type: application/json" -X POST -d '{"username":"admin","password":"'$password'"}' $host/auth | jq ".jwt"`
 token=${token:1}
 token=${token%?}
-data={\"Name\":\"$hostname\",\"URL\":\"tcp://$hostname:2376\",\"PublicURL\":\"$ip\",\"TLS\":true}
+data={\"Name\":\"$hostname\",\"URL\":\"tcp://$ip:2376\",\"PublicURL\":\"$ip\",\"TLS\":true}
 uploadId=`curl -l -H "Content-type: application/json" -H "Authorization: $token" -X POST -d $data $host/endpoints | jq ".Id"`
-curl -F "file=@ca.pem" -H "Authorization: $token" $host/upload/tls/ca?folder=$uploadId
-curl -F "file=@cert.pem" -H "Authorization: $token" $host/upload/tls/cert?folder=$uploadId
-curl -F "file=@key.pem" -H "Authorization: $token" $host/upload/tls/key?folder=$uploadId
+curl -F "file=@/root/tls/pem/ca.pem" -H "Authorization: $token" $host/upload/tls/ca?folder=$uploadId
+curl -F "file=@/root/tls/pem/client-cert.pem" -H "Authorization: $token" $host/upload/tls/cert?folder=$uploadId
+curl -F "file=@/root/tls/pem/client-key.pem" -H "Authorization: $token" $host/upload/tls/key?folder=$uploadId
